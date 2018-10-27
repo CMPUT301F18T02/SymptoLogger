9bce5520175b16a4576c9d654c264ed35485b6f4
PTamm <tamm@ualberta.ca>
2018-10-27 17:49:02 -0600

    Modified tests

--- SymptoLoggerApp/app/src/main/java/com/example/symptologger/Concern.java
+++ SymptoLoggerApp/app/src/main/java/com/example/symptologger/Concern.java
@@ -3,6 +3,14 @@ package com.example.symptologger;
 class Concern {
     private String title;
 
+    Concern(){
+        this.title = null;
+    }
+
+    Concern(String title){
+        this.title = title;
+    }
+
     public void setTitle(String title) {
         this.title = title;
     }
--- SymptoLoggerApp/app/src/test/java/com/example/symptologger/ConcernTest.java
+++ SymptoLoggerApp/app/src/test/java/com/example/symptologger/ConcernTest.java
@@ -1,17 +1,30 @@
 package com.example.symptologger;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class ConcernTest {
 
-    public String testGetTitle(){
-        Concern testConcern = new Concern();
+    @Test // The "@Test" came from https://developer.android.com/training/testing/unit-testing/local-unit-tests#java, 2018-10-27
+    public void testGetTitle(){
+        String test = "Problem";
+        Concern testConcern = new Concern(test);
 
-        return testConcern.getTitle();
+        String testTitle = testConcern.getTitle();
+
+        assertEquals(testTitle,test);
     }
 
-    public void testSetTitle(String title){
+    @Test
+    public void testSetTitle(){
 
         Concern testConcern = new Concern();
 
+        String title = "Problem1";
         testConcern.setTitle(title);
+        String testTitle = testConcern.getTitle();
+
+        assertEquals(title, testTitle);
     }
 }
