73e4ed06ef19bc3484a8c14bf2063b0c14e93968
PTamm <tamm@ualberta.ca>
2018-10-27 20:44:43 -0600

    Added some more tests to ConcernTest

--- SymptoLoggerApp/app/src/main/java/com/example/symptologger/Concern.java
+++ SymptoLoggerApp/app/src/main/java/com/example/symptologger/Concern.java
@@ -1,14 +1,22 @@
 package com.example.symptologger;
 
+import java.util.Date;
+
 class Concern {
     private String title;
+    private Date date;
+    private String description;
 
     Concern(){
-        this.title = null;
+        this.title = "No title given";
+        this.date = new Date();
+        this.description = "No description given";
     }
 
-    Concern(String title){
+    Concern(String title, Date date, String description){
         this.title = title;
+        this.date = date;
+        this.description = description;
     }
 
     public void setTitle(String title) {
@@ -17,5 +25,21 @@ class Concern {
 
     public String getTitle() {
         return this.title;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
--- SymptoLoggerApp/app/src/test/java/com/example/symptologger/ConcernTest.java
+++ SymptoLoggerApp/app/src/test/java/com/example/symptologger/ConcernTest.java
@@ -2,6 +2,8 @@ package com.example.symptologger;
 
 import org.junit.Test;
 
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 public class ConcernTest {
@@ -9,7 +11,7 @@ public class ConcernTest {
     @Test // The "@Test" came from https://developer.android.com/training/testing/unit-testing/local-unit-tests#java, 2018-10-27
     public void testGetTitle(){
         String test = "Problem";
-        Concern testConcern = new Concern(test);
+        Concern testConcern = new Concern(test, new Date(), null);
 
         String testTitle = testConcern.getTitle();
 
@@ -26,5 +28,58 @@ public class ConcernTest {
         String testTitle = testConcern.getTitle();
 
         assertEquals(title, testTitle);
+    }
+
+
+    @Test
+    public void testGetDate(){
+
+        Date date = new Date();
+        String problem = "Problem";
+        Concern newConcern = new Concern(problem, date, null);
+
+        Date testDate = newConcern.getDate();
+
+        assertEquals(date, testDate);
+    }
+
+    @Test
+    public void testSetDate(){
+        Concern myConcern = new Concern("Problem2",null, null);
+
+        Date myDate = new Date();
+
+        myConcern.setDate(myDate);
+
+        Date testDate = myConcern.getDate();
+
+        assertEquals(testDate,myDate);
+    }
+
+    @Test
+    public void testGetDescription(){
+        String description = "My description";
+        Concern concern = new Concern("abc",new Date(), description);
+
+        String d = concern.getDescription();
+
+        assertEquals(description, d);
+    }
+
+    @Test
+    public void testSetDescription(){
+        String description = "A sample description";
+
+        Concern concern = new Concern();
+
+        String des = concern.getDescription();
+
+        assertNotEquals(des,description);
+
+        concern.setDescription(description);
+
+        String newDes = concern.getDescription();
+
+        assertEquals(description,newDes);
     }
 }
