485bfa4ab8b311377586a888788a27dee785df96
Kaiwenkevinz <kevin.zhangkw@gmail.com>
2018-11-01 11:51:16 -0600

    create test

---
+++ SymptoLoggerApp/app/src/test/java/com/example/symptologger/CareProviderTest.java
@@ -0,0 +1,22 @@
+package com.example.symptologger;
+
+import org.junit.Test;
+
+public class CareProviderTest{
+
+    @Test
+    public void testGetPatients(){
+
+    }
+
+    @Test
+    public void testGetConcerns(){
+
+    }
+
+    @Test
+    public void testGetRecords(){
+
+    }
+
+}
