ed0943a0c95f7ec4245b5e419be5bc77efd971d0
PTamm <tamm@ualberta.ca>
2018-10-31 20:27:36 -0600

    commented out getting/setting comments

--- SymptoLoggerApp/app/src/main/java/com/example/symptologger/Record.java
+++ SymptoLoggerApp/app/src/main/java/com/example/symptologger/Record.java
@@ -3,26 +3,26 @@ package com.example.symptologger;
 import java.util.Date;
 
 class Record {
-    private String comment;
+    //private String comment;
     private Date date;
 
     public Record(){
-        this.comment = "";
+        //this.comment = "";
         this.date = new Date();
     }
 
     public Record(String comment, Date date) {
-        this.comment = comment;
+      //  this.comment = comment;
         this.date = date;
     }
 
-    public String getComment() {
-        return this.comment;
-    }
+    //public String getComment() {
+      //  return this.comment;
+    //}
 
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
+    //public void setComment(String comment) {
+      //  this.comment = comment;
+    //}
 
     public Date getDate() {
         return this.date;
