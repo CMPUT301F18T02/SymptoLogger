438f975491ec97e5d33b4534a9a251fef2e7a14f
PTamm <tamm@ualberta.ca>
2018-11-11 18:08:10 -0700

    changed layout

--- SymptoLoggerApp/app/src/main/java/com/example/symptologger/ListConcernActivity.java
+++ SymptoLoggerApp/app/src/main/java/com/example/symptologger/ListConcernActivity.java
@@ -1,9 +1,12 @@
 package com.example.symptologger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -25,5 +28,15 @@ public class ListConcernActivity extends AppCompatActivity {
 
         concernListView = (ListView) findViewById(R.id.listConcernsView);
         concernListAdapter = new ArrayAdapter<Concern>(this, android.R.layout.simple_list_item_1, concernList);
+    }
+
+    public void addConcern(View view){
+        Toast.makeText(this,"Add New Concern", Toast.LENGTH_SHORT).show();
+        Intent newConcernIntent = new Intent(ListConcernActivity.this,NewConcernActivity.class);
+        startActivity(newConcernIntent);
+    }
+
+    public void logOut(View view){
+        Toast.makeText(this,"Good Bye! ...", Toast.LENGTH_SHORT).show();
     }
 }
