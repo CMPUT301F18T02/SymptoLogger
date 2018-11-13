fc41316dc48b0068e981665006b70eb16c4c3910
PTamm <tamm@ualberta.ca>
2018-11-12 22:46:33 -0700

    modify concern activity

--- SymptoLoggerApp/app/src/main/java/com/example/symptologger/ModifyConcernActivity.java
+++ SymptoLoggerApp/app/src/main/java/com/example/symptologger/ModifyConcernActivity.java
@@ -3,6 +3,8 @@ package com.example.symptologger;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class ModifyConcernActivity extends AppCompatActivity {
 
@@ -13,5 +15,14 @@ public class ModifyConcernActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String concernToModify = intent.getStringExtra(ListConcernActivity.EXTRA_MESSAGE);
+    }
+
+    public void modifyConcern(View view){
+
+    }
+
+    public void cancel(View view){
+        Toast.makeText(this,"Cancel ...",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(ModifyConcernActivity.this, ListConcernActivity.class);
     }
 }
