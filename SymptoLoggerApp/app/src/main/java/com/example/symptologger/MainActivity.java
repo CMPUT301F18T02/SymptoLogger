87fe70b63d056ce1a329a009b8d46d2e3896dee0
Kaiwenkevinz <kevin.zhangkw@gmail.com>
2018-11-13 17:09:38 -0700

    added google map api

--- SymptoLoggerApp/app/src/main/java/com/example/symptologger/MainActivity.java
+++ SymptoLoggerApp/app/src/main/java/com/example/symptologger/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +14,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button_sign_up = (Button) findViewById(R.id.button_sign_up);
+        Button button_add_geo_location = (Button) findViewById(R.id.button_geo_location);
 
         button_sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, CreateProfileActivity.class));
+            }
+        });
+
+        button_add_geo_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
             }
         });
     }
