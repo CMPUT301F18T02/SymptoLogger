2f5b6a83dfdf437423d8039ca3c679f77c2c6fc8
dec9k <jjlee3@ualberta.ca>
2018-11-16 12:30:09 -0700

    Photograph Adding completed

--- TestApp/app/src/main/java/com/example/jjlee3/testapp/MainActivity.java
+++ TestApp/app/src/main/java/com/example/jjlee3/testapp/MainActivity.java
@@ -13,22 +13,42 @@ import android.widget.GridView;
 import android.widget.ImageView;
 
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
 
     Record record = new Record();
+    ArrayList<ImageView> views = new ArrayList<>();
     ImageView targetImage;
-    GridView grid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        targetImage = (ImageView)findViewById(R.id.targetimage);
-        //GridView lists the images
-        grid = (GridView)findViewById(R.id.gvgall);
+        targetImage = (ImageView)findViewById(R.id.targetimage1);
+        views.add(targetImage);
+        targetImage = (ImageView)findViewById(R.id.targetimage2);
+        views.add(targetImage);
+        targetImage = (ImageView)findViewById(R.id.targetimage3);
+        views.add(targetImage);
+        targetImage = (ImageView)findViewById(R.id.targetimage4);
+        views.add(targetImage);
+        targetImage = (ImageView)findViewById(R.id.targetimage5);
+        views.add(targetImage);
+        targetImage = (ImageView)findViewById(R.id.targetimage6);
+        views.add(targetImage);
+        targetImage = (ImageView)findViewById(R.id.targetimage7);
+        views.add(targetImage);
+        targetImage = (ImageView)findViewById(R.id.targetimage8);
+        views.add(targetImage);
+        targetImage = (ImageView)findViewById(R.id.targetimage9);
+        views.add(targetImage);
+        targetImage = (ImageView)findViewById(R.id.targetimage10);
+        views.add(targetImage);
+
+
         //This opens up the photo storage and gets displays the images
         Button buttonLoadImage = (Button)findViewById(R.id.addp);
         buttonLoadImage.setOnClickListener(new Button.OnClickListener() {
@@ -42,6 +62,37 @@ public class MainActivity extends AppCompatActivity {
         });
         /////
     }
+
+    public void setImage(Uri uri, ImageView image){
+        Bitmap bitmap;
+
+        try {
+            bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+            image.setImageBitmap(bitmap);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            //Set image as an error image
+            image.setImageBitmap(null);
+
+        }
+    }
+
+    public void DisplayPhotos(){
+        ArrayList<Photograph> ALLPHOTOS = record.getPhoto();
+        for (int i = 0; i < 10; i++){
+            ImageView theview = views.get(i);
+            if (ALLPHOTOS.size() >= i+1){
+                Photograph thephoto = ALLPHOTOS.get(i);
+                setImage(thephoto.getURL(), theview);
+            }else{
+                theview.setImageBitmap(null);
+                //setImage(null, theview);
+            }
+        }
+
+    }
+
 
     @Override
     protected  void onResume() {
@@ -60,7 +111,6 @@ public class MainActivity extends AppCompatActivity {
             Uri uri = data.getData();
             Bitmap bitmap;
 
-
             try {
                 bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
                 //Add photo to the record class
@@ -68,11 +118,13 @@ public class MainActivity extends AppCompatActivity {
                 Photograph photograph = new Photograph(uri);
                 record.addPhoto(photograph);
                 ///////////////////////
-                //targetImage.setImageBitmap(bitmap);
+                targetImage.setImageBitmap(bitmap);
             } catch (FileNotFoundException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
+
+            DisplayPhotos();
         }
     }
 }
