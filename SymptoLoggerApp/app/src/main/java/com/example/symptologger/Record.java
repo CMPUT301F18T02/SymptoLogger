9f65ed26de3797575fb98a44188bd8b6069b3298
PTamm <tamm@ualberta.ca>
2018-11-20 01:30:39 -0700

    added javadocs to Concern and Record classes

--- SymptoLoggerApp/app/src/main/java/com/example/symptologger/Record.java
+++ SymptoLoggerApp/app/src/main/java/com/example/symptologger/Record.java
@@ -7,18 +7,37 @@ import com.google.android.gms.maps.GoogleMap;
 import java.util.ArrayList;
 import java.util.Date;
 
+
+/**
+ * The record class. A record can contain photos and other details pertaining to a concern.
+ *
+ * @author Patrick Tamm
+ * @see Concern, RecordList,
+ */
+
 class Record {
     //private String comment;
     private Date date;
     private Location location;
     private String title;
 
+    /**
+     * The first of two constructors for the Record class. If no title or
+     * date are supplied, this constructor is used.
+     */
+
     public Record(){
-        //this.comment = "";
+        this.title = "";
         this.date = new Date();
     }
 
-    public Record(/*String comment,*/ Date date, String title) {
+    /**
+     * optional title and date are supplied to constructor.
+     * @param date the date value for the record
+     * @param title the title given to the record
+     */
+
+    public Record(Date date, String title) {
       //  this.comment = comment;
         this.date = date;
         this.title = title;
@@ -32,35 +51,74 @@ class Record {
       //  this.comment = comment;
     //}
 
+    /**
+     * returns the title of the record
+     * @return title
+     */
+
     public String getTitle(){
         return this.title;
     }
+
+    /**
+     * used when displaying records, such as in a ListView.
+     * @return title
+     */
 
     public String toString(){
         return this.title;
     }
 
+    /**
+     * gets the date of the record
+     * @return date
+     */
+
     public Date getDate() {
         return this.date;
     }
+
+    /**
+     * sets the date for a previously created record
+     * @param date the new date value
+     */
 
     public void setDate(Date date) {
         this.date = date;
     }
 
+    /**
+     * adding photos to records.
+     */
+
     public void addPhoto(){
         //When photo functionality established
     }
 
-    public ArrayList<Photograph> getPhoto() {
+    /**
+     * gets the photos associated with a record.
+     * @return list of photos
+     */
+
+    public ArrayList<Photograph> getPhotos() {
         //When photo functionality established
 
         return new ArrayList<>();
     }
 
+
+    /**
+     * adding a geolocation to a record
+     */
+
     public void addGeoLocation(){
         //When location services established and enabled
     }
+
+    /**
+     * getting a geolocation from a record.
+     * @return geolocation
+     */
 
     public Location getGeoLocation() {
         //When location services is up
@@ -68,19 +126,37 @@ class Record {
         return this.location;
     }
 
+    /**
+     * adding comments made by care providers assigned to patient.
+     */
+
     public void addCareProviderComment(){
         //When CareProviderComment is ready
     }
+
+    /**
+     * getting comments made by care providers assigned to patient
+     * @return care provider comments
+     */
 
     public ArrayList<CareProviderComment> getCareProviderComment() {
         //When CareProviderComment is ready
         return new ArrayList<>();
     }
 
+    /**
+     * adding a patient comment
+     */
+
     public void addPatientComment(){
         //When patient comment is ready
     }
 
+    /**
+     * returns the comments made by patients
+     * @return patient comments
+     */
+    
     public ArrayList<PatientComment> getPatientComment() {
         //When patient comment is ready
         return new ArrayList<>();
