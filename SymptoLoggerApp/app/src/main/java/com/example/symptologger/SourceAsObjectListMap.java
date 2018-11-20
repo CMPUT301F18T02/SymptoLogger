2e46744e36e2c7aa264f0980c5ea3485a2e8e52c
PTamm <tamm@ualberta.ca>
2018-11-20 12:31:38 -0700

    javadoc

--- SymptoLoggerApp/app/src/main/java/com/example/symptologger/SourceAsObjectListMap.java
+++ SymptoLoggerApp/app/src/main/java/com/example/symptologger/SourceAsObjectListMap.java
@@ -1,10 +1,27 @@
 package com.example.symptologger;
 
+/**
+ * Used for mapping values fro ElasticSearch
+ *
+ * @author Remi Arshad
+ */
+
 public class SourceAsObjectListMap {
     private String userID;
     private Integer memberID;
 
+    /**
+     * Gets the user id
+     * @return userID
+     */
+
     public String getUserID() { return userID; }
+
+    /**
+     * Gets the member id
+     * @return memberID
+     */
+
     public Integer getMemberID() {
         return memberID;
     }
