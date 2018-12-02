9c6a5d5df239451df37f374770f92bc393728a47
ne-v0y <voyageryifan@gmail.com>
2018-12-02 13:33:38 -0700

    wip - incorporate chat

--- SymptoLoggerApp/app/src/main/java/com/example/symptologger/RecordCommentFragment.java
+++ SymptoLoggerApp/app/src/main/java/com/example/symptologger/RecordCommentFragment.java
@@ -3,14 +3,21 @@ package com.example.symptologger;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
 
 /*
  *  Copyright 2018 Remi Arshad, Noni Hua, Jason Lee, Patrick Tamm, Kaiwen Zhang
@@ -47,6 +54,22 @@ public class RecordCommentFragment extends Fragment {
 
     Record record;
 
+    private TextView textBox;
+    private EditText messageBox;
+
+    private String senderID = "test1234";
+    private String receiverID = "patrick1";
+    private String message;
+    private Date date;
+
+    public static boolean updateLogsReady = false;
+    public static List<List<ChatLogs>> chatLogs = new ArrayList<>();
+
+    private static RecyclerView recyclerView;
+    private ChatManager cm;
+    private static ChatViewAdapter mAdapter;
+
+
     public RecordCommentFragment() {
         // Required empty public constructor
     }
@@ -79,15 +102,28 @@ public class RecordCommentFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_record_comment, container, false);
 
-        record.addCareProviderComment();
-        careProviderCommentList = record.getCareProviderComment();
+        messageBox = view.findViewById(R.id.addComment);
+        textBox = view.findViewById(R.id.chatbox);
 
-        adapter = new ArrayAdapter<CareProviderComment>(
-                getContext(),
-                R.layout.list_layout,
-                careProviderCommentList);
-        ListView careProviderCommentsListView = view.findViewById(R.id.recordCommentList);
-        careProviderCommentsListView.setAdapter(adapter);
+        recyclerView = view.findViewById(R.id.chatlogs_holder);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new ChatViewAdapter(chatLogs);
+        recyclerView.setAdapter(mAdapter);
+
+        cm = new ChatManager(getActivity());
+        cm.startFetchLogsTimer();
+
+
+//        record.addCareProviderComment();
+//        careProviderCommentList = record.getCareProviderComment();
+//
+//        adapter = new ArrayAdapter<CareProviderComment>(
+//                getContext(),
+//                R.layout.list_layout,
+//                careProviderCommentList);
+//        ListView careProviderCommentsListView = view.findViewById(R.id.recordCommentList);
+//        careProviderCommentsListView.setAdapter(adapter);
 
         return view;
     }
@@ -110,11 +146,36 @@ public class RecordCommentFragment extends Fragment {
 //        }
 //    }
 //
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
+    @Override
+    public void onDetach() {
+        super.onDetach();
 //        mListener = null;
-//    }
+        cm.stopFetchLogsTimer();
+    }
+
+    public void send(View v) { //USE LISTENER METHOD INSTEAD ON FINAL PROJECT
+        cm.stopFetchLogsTimer();
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss.SSS dd/MM/yyyy");
+        formatter.setTimeZone(TimeZone.getTimeZone("MST"));
+        date = new Date();
+
+        message = messageBox.getText().toString();
+        System.out.println(message);
+
+        ChatManager.setSaveVars(senderID, receiverID, message, formatter.format(date));
+        ChatManager.saveLogs();
+        cm.restartTimer();
+    }
+
+    public static void updateView() {
+        System.out.println("-----status-----");
+        System.out.println(updateLogsReady);
+        if (updateLogsReady == true) {
+            updateLogsReady = false;
+            mAdapter.notifyDataSetChanged();
+            recyclerView.getLayoutManager().smoothScrollToPosition(recyclerView, null, mAdapter.getItemCount()-1);
+        }
+    }
 //
 //    public interface OnFragmentInteractionListener {
 //        // TODO: Update argument type and name
