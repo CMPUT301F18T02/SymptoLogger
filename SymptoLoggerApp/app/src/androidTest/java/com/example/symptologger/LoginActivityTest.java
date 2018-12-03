1326f52a3e721d89e43e59899d1df41e3bb6fe26
ne-v0y <voyageryifan@gmail.com>
2018-12-03 14:08:28 -0700

    remove dead tests, added login page test

--- SymptoLoggerApp/app/src/androidTest/java/com/example/symptologger/LoginActivityTest.java
+++ SymptoLoggerApp/app/src/androidTest/java/com/example/symptologger/LoginActivityTest.java
@@ -17,8 +17,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -26,33 +26,42 @@ import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class NewRecordActivityAddLocationTest {
+public class LoginActivityTest {
 
     @Rule
-    public ActivityTestRule<NewRecordActivity> mActivityTestRule = new ActivityTestRule<>(NewRecordActivity.class);
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void newRecordActivityAddLocationTest() {
+    public void loginActivityTest() {
         ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.addLocationButton), withText("Add location"),
+                allOf(withId(R.id.sendCommentButton), withText("Sign In"),
                         childAtPosition(
                                 childAtPosition(
                                         withId(android.R.id.content),
                                         0),
-                                6),
+                                1),
                         isDisplayed()));
         appCompatButton.perform(click());
 
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        ViewInteraction button = onView(
+                allOf(withId(R.id.button3),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        button.check(matches(isDisplayed()));
 
-        pressBack();
+//        ViewInteraction editText = onView(
+//                allOf(withId(R.id.typedUserName), withText("Username"),
+//                        childAtPosition(
+//                                childAtPosition(
+//                                        withId(android.R.id.content),
+//                                        0),
+//                                0),
+//                        isDisplayed()));
+//        editText.check(matches(isDisplayed()));
 
     }
 
